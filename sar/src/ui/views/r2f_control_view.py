904d6fc9e19b96ddd11f37f21da97ccf9da21c70
Domingo Ramos <dramos.bold@gmail.com>
2026-09-24 17:19:43 -0500

    Proyecto SARok_CancunBot v03

--- sar/src/ui/views/r2f_control_view.py
+++
@@ -1,5 +0,0 @@
-"""R2F Control View (Re-export Wrapper for Backward Compatibility)."""
-
-from cancunbot.src.ui.views.r2f_control_view import R2FControlView, R2FLoadWorker
-
-__all__ = ["R2FControlView", "R2FLoadWorker"]
